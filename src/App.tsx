ba3d43e57d675a74378258e3fa2fdcdd1a6f6aea
DANAB\danab <danabrasanac@gmail.com>
2023-03-03 16:15:22 +0100

    Fixed prettier config, created basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import { HomePage } from './pages/HomePage.component'
+import { BookDetails } from './pages/BookDetails.component'
+import { FooterMenu } from './components/FooterMenu.component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header' />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='details' element={<BookDetails />} />
+      </Routes>
+      <FooterMenu />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
