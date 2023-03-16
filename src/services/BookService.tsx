32c4d3f034df1a1f3f1baf46f9711e7d2f04dcac
DANAB\danab <danabrasanac@gmail.com>
2023-03-16 10:14:14 +0100

    added check for url in api calls

--- src/services/BookService.tsx
+++ src/services/BookService.tsx
@@ -2,4 +2,7 @@ import axios from 'axios'
 
 const url: string = process.env.REACT_APP_API_URL ?? ''
 
-export const createBook = async (formData:FormData) => axios.post(`${url}Books`, formData)
+export const createBook = async (formData:FormData) => {
+  if(url) return axios.post(`${url}Books`, formData)
+  return Promise.reject(Error('URL not valid'))
+}
